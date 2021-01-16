87c987c0f4f7b504ce66d41440400d104033e83f
Samir <sam271091@gmail.com>
2021-01-16 22:43:56 +0400

    Commit

--- app/src/main/java/com/example/mywallet/transaction_item.java
+++ app/src/main/java/com/example/mywallet/transaction_item.java
@@ -4,13 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -20,7 +24,7 @@ public class transaction_item extends AppCompatActivity {
     private Wallet wallet;
     private ImageView imageViewType;
     private Type type;
-    private EditText editTextDate;
+    private TextView textViewDate;
     private TextView textViewWallet;
     private TextView textViewValueItem;
     private TextView textViewCounterparty;
@@ -30,19 +34,26 @@ public class transaction_item extends AppCompatActivity {
     private double sum;
     private Date date;
 
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private int year, month, day;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_item);
 
+        calendar = Calendar.getInstance();
+
         if (date == null){
-            date = Calendar.getInstance().getTime();
+            date = calendar.getTime();
         }
 
+        setDateValues();
 
 
         imageViewType = findViewById(R.id.imageViewType);
-        editTextDate =  findViewById(R.id.editTextDate);
+        textViewDate =  findViewById(R.id.textViewDate);
         textViewWallet = findViewById(R.id.textViewWallet);
         textViewValueItem = findViewById(R.id.textViewValueItem);
         textViewCounterparty = findViewById(R.id.textViewCounterparty);
@@ -82,10 +93,27 @@ public class transaction_item extends AppCompatActivity {
         });
 
 
-        editTextDate.setText(date.toString());
+//        textViewDate.setText(date.toString());
+
+        showDate();
+
+        textViewDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setDate();
+            }
+        });
 
     }
 
+    void setDateValues(){
+        calendar.setTime(date);
+
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -123,6 +151,8 @@ public class transaction_item extends AppCompatActivity {
 
         date = (Date) savedInstanceState.getSerializable("date");
 
+        setDateValues();
+
         sum = savedInstanceState.getDouble("sum");
         valueItem = (ValueItem) savedInstanceState.getSerializable("valueItem");
         counterparty = (Counterparty) savedInstanceState.getSerializable("counterparty");
@@ -134,8 +164,56 @@ public class transaction_item extends AppCompatActivity {
             textViewCounterparty.setText(counterparty.getName());
         }
 
-        editTextDate.setText(date.toString());
+//        textViewDate.setText(date.toString());
+
+        showDate();
     }
 
+
+
+
+    @SuppressWarnings("deprecation")
+    public void setDate() {
+        showDialog(999);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker,
+                                      int arg1, int arg2, int arg3) {
+
+                     day = datePicker.getDayOfMonth();
+                     month = datePicker.getMonth();
+                     year =  datePicker.getYear();
+
+                    calendar.set(year, month, day);
+
+                    date = calendar.getTime();
+
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate();
+                }
+            };
+
+    private void showDate() {
+
+
+        textViewDate.setText(new StringBuilder().append(day).append("/")
+                .append(month+1).append("/").append(year));
+    }
 
 }
